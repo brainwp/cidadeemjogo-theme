6b0267eedd8ec4ca0cc92682981b0ffa994b08d2
Diego Rojas <diego@brasa.art.br>
2017-09-08 15:47:35 -0300

    adicionando opção para URL do botão jogar

--- functions.php
+++ functions.php
@@ -4,6 +4,44 @@
  *
  */
 
+// Insere função no hook do tema, originalmente no arquivo inc/customize.php
+
+function coletivo_customize_after_register($wp_customize){
+
+    /*------------------------------------------------------------------------*/
+    /*  Site Options
+    /*------------------------------------------------------------------------*/
+        $wp_customize->add_section( 'coletivo_play_settings',
+            array(
+                'priority'       => 20,
+                'capability'     => 'edit_theme_options',
+                'theme_supports' => '',
+                'title'          => esc_html__( 'Link Jogar', 'coletivo' ),
+                'description'    => '',
+                'panel'          => 'coletivo_options',
+            )
+        );
+
+        // Play custom Link
+        $wp_customize->add_setting( 'coletivo_jogar_text_link',
+            array(
+                'sanitize_callback' => 'esc_url',
+                'default'           => esc_url( home_url( '/' )).esc_html__('', 'coletivo'),
+                'transport'         => 'refresh',
+            )
+        );
+        $wp_customize->add_control( 'coletivo_jogar_text_link',
+            array(
+                'type'  =>'text',
+                'label'       => esc_html__('URL do Link de Jogar', 'coletivo'),
+                'section'     => 'coletivo_play_settings',
+                'description' => ''
+            )
+        );
+    }
+
+add_action( 'coletivo_customize_after_register', 'coletivo_customize_after_register' );
+
 // Altera a função do header, originalmente no arquivo inc/template-tags.php
 
 if ( ! function_exists( 'coletivo_site_header' ) ) {
@@ -21,8 +59,14 @@ if ( ! function_exists( 'coletivo_site_header' ) ) {
                 </div>
                 <!-- .site-branding -->
                 <div class="header-right-wrapper">
+                <?php
+                    $coletivo_jogar_text_link = get_theme_mod( 'coletivo_jogar_text_link' );
+                ?>
                     <a href="#0" id="nav-toggle"><?php _e('Menu', 'coletivo'); ?><span></span></a>
-                    <div class="btn-play alignright"><a class="btn btn-theme-primary" onclick="goog_report_conversion ('http://13.58.122.3/cidadeemjogo/')" href="http://13.58.122.3/cidadeemjogo/">Jogar</a></div>
+                    <div class="btn-play alignright">
+                        <a class="btn btn-theme-primary" onclick="goog_report_conversion ('<?php  if ($coletivo_jogar_text_link != '') echo $coletivo_jogar_text_link; ?>')" href="<?php  if ($coletivo_jogar_text_link != '') echo $coletivo_jogar_text_link; ?>">Jogar
+                        </a>
+                    </div>
                     <nav id="site-navigation" class="main-navigation alignright" role="navigation">
                         <ul class="coletivo-menu">
                             <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => '', 'items_wrap' => '%3$s')); ?>
