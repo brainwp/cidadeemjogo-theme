3f227637a97cd2fd7160be2e30f454d1107b5ea4
Diego Rojas <diego@brasa.art.br>
2017-12-18 12:04:44 -0200

    fix function bug for fb image

--- functions.php
+++ functions.php
@@ -92,23 +92,13 @@ add_filter('language_attributes', 'add_opengraph_doctype');
 //Lets add Open Graph Meta Info
  
 function insert_fb_in_head() {
-    global $post;
-    if ( !is_singular()) //if it is not a post or a page
+    if ( is_front_page()) //if it is home page
         return;
-        echo '<meta property="fb:admins" content="B6shWOjvfTZ"/>';
+        echo '<meta property="fb:admins" content="A4NjnfunWGV"/>';
         echo '<meta property="og:title" content="' . get_the_title() . '"/>';
         echo '<meta property="og:type" content="article"/>';
         echo '<meta property="og:url" content="' . get_permalink() . '"/>';
-        echo '<meta property="og:site_name" content=" ' . get_bloginfo() . '"/>';
-    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
-        $default_image="http://cidadeemjogo.org.br/wp-content/uploads/2017/08/CIDADE-EM-JOGO-logo-export.jpg";
-        echo '<meta property="og:image" content="' . $default_image . '"/>';
-    }
-    else{
-        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
-        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
-    }
-    echo "
-";
+        echo '<meta property="og:site_name" content="CIDADE EM JOGO"/>';
+	    echo '<meta property="og:image" content="http://cidadeemjogo.org.br/wp-content/uploads/2017/08/CIDADE-EM-JOGO-logo-export.jpg"/>';
 }
 add_action( 'wp_head', 'insert_fb_in_head', 5 );
